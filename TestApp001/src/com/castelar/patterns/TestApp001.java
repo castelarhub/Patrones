c4ee38bcfba3d09e3649272fc2dd0acf1e324762
operador <operador@10.0.2.15>
2014-07-23 11:37:31 -0500

    Validando accion de modificacion.

--- TestApp001/src/com/castelar/patterns/TestApp001.java
+++ TestApp001/src/com/castelar/patterns/TestApp001.java
@@ -17,7 +17,7 @@ public class TestApp001 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("Just to verify app..");
+        System.out.print("Just to verify app.. updated...");
                 
         
     }
