a5931d4ce293654469c078a6c38028dc21f03091
operador <operador@10.0.2.15>
2014-07-24 19:46:47 -0500

    Se crea el proyecto para ejemplificar el patron Factory. Se crea una interfaz y unas clases concretas que implementan esta interfaz.

---
+++ FactoryPatternApp003/src/com/castelar/patterns/client/FactoryPatternDemo.java
@@ -0,0 +1,20 @@
+/*
+ * Ejemplo de implementacion del patron de diseño factory
+ */
+
+package com.castelar.patterns.client;
+
+/**
+ *
+ * @author operador
+ */
+public class FactoryPatternDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
